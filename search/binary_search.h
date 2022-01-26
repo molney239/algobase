352d0500d078c17f5087ee9c68786aa55cf81778
Timofey <molney239@gmail.com>
2022-01-26 10:18:49 +0300

    Ternary search

--- search/binary_search.h
+++ search/binary_search.h
@@ -1,8 +1,6 @@
 #ifndef ALGOBASE_BINARY_SEARCH_H
 #define ALGOBASE_BINARY_SEARCH_H
 
-#include <iostream>
-
 template<typename T>
 T binary_search(T l, T r, T precision, bool (*good) (T)) {
     T mid;
