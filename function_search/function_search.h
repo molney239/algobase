eadbe694a419f07d7b7484ebde8bb92c53a7dc15
Timofey <molney239@gmail.com>
2022-03-08 11:25:55 +0300

    Updated DFS and BFS

--- function_search/function_search.h
+++ function_search/function_search.h
@@ -5,28 +5,6 @@ namespace function_search {
 
 
     /**
-     *
-     * Searches least argument in range [l; r] for which function returns true.
-     *
-     * @tparam T Type of function argument
-     * @param l Left range bound
-     * @param r Right range bound
-     * @param step Step for search
-     * @param good Function for search
-     * @return Least argument in range [l; r] for which function returns true
-     */
-    template<typename T>
-    T linear_search(T l, T r, T step, bool (*good) (T)) {
-        T pos = l;
-        while (pos < r) {
-            if (good(pos)) return pos;
-            pos += step;
-        }
-        return r;
-    }
-
-
-    /**
      * Searches least argument in range [l; r] for which function returns true.
      *
      * @tparam T Type of function argument
@@ -53,28 +31,6 @@ namespace function_search {
      * Searches least argument in range [l; r] for which monotonically increasing function returns true.
      *
      * @tparam T Type of function argument
-     * @param l Left range bound
-     * @param r Right range bound
-     * @param precision Precision for binary search bounds
-     * @param good Monotonically increasing function for search
-     * @return Least argument in range [l; r] for which function returns true
-     */
-    template<typename T>
-    T binary_search(T l, T r, T precision, bool (*good) (T)) {
-        T mid;
-        while (r - l > precision) {
-            mid = l + (r - l) / 2;
-            if (good(mid)) r = mid;
-            else l = mid + 1;
-        }
-        return r;
-    }
-
-
-    /**
-     * Searches least argument in range [l; r] for which monotonically increasing function returns true.
-     *
-     * @tparam T Type of function argument
      * @tparam Args Parameter pack for additional function arguments
      * @param l Left range bound
      * @param r Right range bound
@@ -90,31 +46,6 @@ namespace function_search {
             mid = l + (r - l) / 2;
             if (good(mid, args...)) r = mid;
             else l = mid + 1;
-        }
-        return r;
-    }
-
-
-    /**
-     * Searches for the function argument for which it reaches its minimum on range [l; r].
-     * The function must first be monotonically decreasing and then monotonically increasing.
-     *
-     * @tparam T1 Type of function argument
-     * @tparam T2 Function return type
-     * @param l Left range bound
-     * @param r Right range bound
-     * @param precision Precision for ternary search bounds
-     * @param func Function for search
-     * @return Function argument at which it reaches its minimum
-     */
-    template<typename T1, typename T2>
-    T1 ternary_search(T1 l, T1 r, T1 precision, T2 (*func) (T1)) {
-        T1 mid1, mid2;
-        while (r - l > precision * 3) {
-            mid1 = l + (r - l) / 3.0;
-            mid2 = r - (r - l) / 3.0;
-            if (func(mid1) < func(mid2)) l = mid1;
-            else r = mid2;
         }
         return r;
     }
