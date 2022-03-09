50c5486ce098a959f5585e639d237ae3f57af8df
Timofey <molney239@gmail.com>
2022-03-09 17:31:28 +0300

    Rewrote segment tree

--- trees/segment_tree.h
+++ trees/segment_tree.h
@@ -3,156 +3,89 @@
 
 #include <vector>
 
-namespace segment_tree {
+template<typename T>
+struct segment_tree {
 
-    template<typename T>
-    T sum(T a, T b) {
-        return a + b;
+    std::vector<T> tree, mark;
+    std::vector<bool> is_marked;
+
+    segment_tree(const std::vector<T>& v) {
+        tree.assign(v.size() * 4, 0);
+        mark.assign(v.size() * 4, 0);
+        is_marked.assign(v.size() * 4, false);
+        this->size = v.size();
+        build(v, 0, 0, size - 1);
     }
 
-    template<typename T>
-    void build(const std::vector<T>& v, std::vector<T>& tree, unsigned pos, unsigned l, unsigned r,
-               T (*func)(T, T)) {
-        if (l == r) tree[pos] = v[l];
-        else {
-            unsigned m = l + (r - l) / 2;
-            build(v, tree, pos * 2 + 1, l, m, func);
-            build(v, tree, pos * 2 + 2, m + 1, r, func);
-            tree[pos] = func(tree[pos * 2 + 1], tree[pos * 2 + 2]);
-        }
+    T get(unsigned l, unsigned r) {
+        if (l > r) return 0;
+        return get(l, min(r, size - 1), 0, 0, size - 1);
     }
 
-    template<typename T>
-    void build(const std::vector<T>& v, std::vector<T>& tree, T (*func)(T, T) = sum) {
-        tree.resize(v.size() * 4);
-        build(v, tree, 0, 0, v.size() - 1, func);
+    void update(unsigned l, unsigned r, T value) {
+        if (l > r) return;
+        update(l, min(r, size - 1), value, 0, 0, size - 1);
     }
 
-    template<typename T>
-    T get(const std::vector<T>& tree, unsigned pos, unsigned l, unsigned r,
-            unsigned tl, unsigned tr,
-            T (*func)(T, T), T neutral) {
-        if (tr == -1) tr = tree.size() - 1;
-        if (l <= tl && tr <= r) return tree[pos];
-        if (r < tl || tr < l) return neutral;
+protected:
 
-        unsigned m = tl + (tr - tl) / 2;
-        return func(get(tree, pos * 2 + 1, l, r, tl, m, func, neutral),
-                    get(tree, pos * 2 + 2, l, r, m + 1, tr, func, neutral));
-    }
+    unsigned size;
 
-    template<typename T>
-    T get(const std::vector<T>& tree, unsigned l, unsigned r,
-          T (*func)(T, T) = sum, T neutral = 0) {
-        return get(tree, 0, l, r, 0, tree.size() / 4 - 1, func, neutral);
-    }
-
-    template<typename T>
-    void update(std::vector<T>& tree, unsigned pos, unsigned l, unsigned r,
-                  unsigned position, T value,
-                  T (*func)(T, T)) {
-        if (l == r && l == position) {
-            tree[pos] = value;
-            return;
-        }
-        if (position < l || r < position) return;
-
-        unsigned m = l + (r - l) / 2;
-        update(tree, pos * 2 + 1, l, m, position, value, func);
-        update(tree, pos * 2 + 2, m + 1, r , position, value, func);
-        tree[pos] = func(tree[pos * 2 + 1], tree[pos * 2 + 2]);
-    }
-
-    template<typename T>
-    void update(std::vector<T>& tree, unsigned position, T value,
-                T (*func)(T, T) = sum) {
-        update(tree, 0, 0, tree.size() / 4 - 1, position, value, func);
-    }
-
-}
-
-namespace lazy_propagation_st {
-
-    template<typename T>
-    T sum(T a, T b) {
-        return a + b;
-    }
-
-    template<typename T>
-    void build(const std::vector<T>& v, std::vector<T>& tree, std::vector<T>& mark,
-               unsigned pos, unsigned l, unsigned r,
-               T plug,
-               T (*func)(T, T)) {
-        mark[pos] = plug;
+    void build(const std::vector<T>& v, int pos, int l, int r) {
         if (l == r) {
             tree[pos] = v[l];
             return;
         }
 
-        T m = (l + r) / 2;
-        build(v, tree, mark, pos * 2 + 1, l, m, plug, func);
-        build(v, tree, mark, pos * 2 + 2, m + 1, r, plug, func);
-        tree[pos] = func(tree[pos * 2 + 1], tree[pos * 2 + 2]);
+        int m = (l + r) / 2;
+        build(v, pos * 2 + 1, l, m);
+        build(v, pos * 2 + 2, m + 1, r);
+        tree[pos] = tree[pos * 2 + 1] + tree[pos * 2 + 2];
     }
 
-    template<typename T>
-    void build(const std::vector<T>& v, std::vector<T>& tree, std::vector<T>& mark,
-               T plug = -1,
-               T (*func)(T, T) = sum) {
-        tree.resize(v.size() * 4);
-        mark.resize(v.size() * 4);
-        build(v, tree, mark, 0, 0, v.size() - 1, plug, func);
-    }
+    void propagate(unsigned pos, unsigned l, unsigned r) {
+        if (!is_marked[pos]) return;
 
-    template<typename T>
-    void propagate(std::vector<T>& tree, std::vector<T>& mark, unsigned pos,
-                   unsigned l, unsigned r, T plug) {
-        if (l == r) {
-            tree[pos] = mark[pos];
-            mark[pos] = plug;
-            return;
+        if (l < r) {
+            unsigned m = (l + r) / 2;
+            tree[pos * 2 + 1] = (m - l + 1) * mark[pos];
+            mark[pos * 2 + 1] = mark[pos];
+            is_marked[pos * 2 + 1] = true;
+
+            tree[pos * 2 + 2] = (r - m) * mark[pos];
+            mark[pos * 2 + 2] = mark[pos];
+            is_marked[pos * 2 + 2] = true;
         }
-        mark[pos * 2 + 1] = mark[pos];
-        mark[pos * 2 + 2] = mark[pos];
-        tree[pos] = (r - l + 1) * mark[pos];
-        mark[pos] = plug;
+
+        is_marked[pos] = false;
     }
 
-    template<typename T>
-    T get(std::vector<T>& tree, std::vector<T>& mark, unsigned l, unsigned r,
-          unsigned pos, unsigned tl, unsigned tr,
-          T neutral, T plug, T (*func)(T, T)) {
-        if (tr < l || r < tl) return neutral;
-        propagate(tree, mark, 0, 0, tree.size() / 4 - 1, plug);
+    T get(unsigned l, unsigned r, unsigned pos, unsigned tl, unsigned tr) {
+        if (tr < l || r < tl) return 0;
+        propagate(pos, tl, tr);
         if (l <= tl && tr <= r) return tree[pos];
 
-        T m = (tl + tr) / 2;
-        return func(get(tree, mark, l, r, pos * 2 + 1, tl, m, neutral, plug, func),
-                    get(tree, mark, l, r, pos * 2 + 2, m + 1, tr, neutral, plug, func));
+        unsigned m = (tl + tr) / 2;
+        return get(l, r, pos * 2 + 1, tl, m) +
+               get(l, r, pos * 2 + 2, m + 1, tr);
     }
 
-    template<typename T>
-    T get(std::vector<T>& tree, std::vector<T>& mark, unsigned l, unsigned r,
-          T neutral = 0, T plug = -1, T (*func)(T, T) = sum) {
-        return get(tree, mark, l, r, 0, 0, tree.size() / 4 - 1, neutral, plug, func);
-    }
-
-    template<typename T>
-    void update(std::vector<T>& tree, std::vector<T>& mark, unsigned l, unsigned r, T value,
-                unsigned pos, unsigned tl, unsigned tr, T (*func)(T, T)) {
+    void update(unsigned l, unsigned r, T value, unsigned pos, unsigned tl, unsigned tr) {
         if (tr < l || r < tl) return;
-
+        propagate(pos, tl, tr);
         if (l <= tl && tr <= r) {
+            tree[pos] = (tr - tl + 1) * value;
             mark[pos] = value;
+            is_marked[pos] = true;
             return;
         }
 
-        T m = (l + r) / 2;
-        update(tree, mark, l, r, value, pos * 2 + 1, tl, m, func);
-        update(tree, mark, l, r, value, pos * 2 + 2, m + 1, tr, func);
-        tree[pos] = func(tree[pos * 2 + 2], tree[pos * 2 + 2]);
+        unsigned m = (tl + tr) / 2;
+        update(l, r, value, pos * 2 + 1, tl, m);
+        update(l, r, value, pos * 2 + 2, m + 1, tr);
+        tree[pos] = tree[pos * 2 + 1] + tree[pos * 2 + 2];
     }
 
-}
+};
 
 #endif //ALGOBASE_SEGMENT_TREE_H
