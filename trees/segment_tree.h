9ff34dfe2e6ab8a2d91cb3ff80f5d93f7bb86e8a
Timofey <molney239@gmail.com>
2022-03-11 16:30:22 +0300

    Dynamic segment tree

--- trees/segment_tree.h
+++ trees/segment_tree.h
@@ -2,6 +2,7 @@
 #define ALGOBASE_SEGMENT_TREE_H
 
 #include <vector>
+#include <unordered_map>
 
 template<typename T>
 struct segment_tree {
@@ -25,6 +26,10 @@ struct segment_tree {
     void update(unsigned l, unsigned r, T value) {
         if (l > r) return;
         update(l, min(r, size - 1), value, 0, 0, size - 1);
+    }
+
+    unsigned get_size() {
+        return this->size;
     }
 
 protected:
@@ -84,6 +89,79 @@ protected:
         update(l, r, value, pos * 2 + 1, tl, m);
         update(l, r, value, pos * 2 + 2, m + 1, tr);
         tree[pos] = tree[pos * 2 + 1] + tree[pos * 2 + 2];
+    }
+
+};
+
+
+template<typename T>
+struct dynamic_segment_tree {
+
+    dynamic_segment_tree(unsigned size) {
+        this->size = size;
+    }
+
+    T get(unsigned l, unsigned r) {
+        if (l > r) return 0;
+        return get(l, min(r, size - 1), 0, 0, size - 1);
+    }
+
+    void update(unsigned l, unsigned r, T value) {
+        if (l > r) return;
+        update(l, min(r, size - 1), value, 0, 0, size - 1);
+    }
+
+    unsigned get_size() {
+        return this->size;
+    }
+
+protected:
+
+    unsigned size;
+
+    std::unordered_map<unsigned, pair<T, pair<bool, T>>> tree;
+
+    void propagate(unsigned pos, unsigned l, unsigned r) {
+        if (!tree[pos].second.first) return;
+
+        if (l < r) {
+            unsigned m = (l + r) / 2;
+            tree[pos * 2 + 1].first = (m - l + 1) * tree[pos].second.second;
+            tree[pos * 2 + 1].second.second = tree[pos].second.second;
+            tree[pos * 2 + 1].second.first = true;
+
+            tree[pos * 2 + 2].first = (r - m) * tree[pos].second.second;
+            tree[pos * 2 + 2].second.second = tree[pos].second.second;
+            tree[pos * 2 + 2].second.first = true;
+        }
+
+        tree[pos].second.first = false;
+    }
+
+    T get(unsigned l, unsigned r, unsigned pos, unsigned tl, unsigned tr) {
+        if (tr < l || r < tl) return 0;
+        propagate(pos, tl, tr);
+        if (l <= tl && tr <= r) return tree[pos].first;
+
+        unsigned m = (tl + tr) / 2;
+        return get(l, r, pos * 2 + 1, tl, m) +
+               get(l, r, pos * 2 + 2, m + 1, tr);
+    }
+
+    void update(unsigned l, unsigned r, T value, unsigned pos, unsigned tl, unsigned tr) {
+        if (tr < l || r < tl) return;
+        propagate(pos, tl, tr);
+        if (l <= tl && tr <= r) {
+            tree[pos].first = (tr - tl + 1) * value;
+            tree[pos].second.second = value;
+            tree[pos].second.first = true;
+            return;
+        }
+
+        unsigned m = (tl + tr) / 2;
+        update(l, r, value, pos * 2 + 1, tl, m);
+        update(l, r, value, pos * 2 + 2, m + 1, tr);
+        tree[pos].first = tree[pos * 2 + 1].first + tree[pos * 2 + 2].first;
     }
 
 };
